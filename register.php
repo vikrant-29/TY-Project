e2fe6993879c98807f476fc0138a1f5ad90be578
AdityaGowari22 <nkadityagowari@gmail.com>
2025-01-12 09:03:28 +0530

    home page added

--- register.php
+++ register.php
@@ -8,11 +8,11 @@
 <body>
 <h2>Create New Account</h2>   
     <form action = user_data.php method="POST">
-    <input type="text" name = "firstName" placeholder="First Name" requrired >
-    <input type ="text" name = "lastName" palceholder="Last Name" required> 
-    <input type ="text" name = "userName" palceholder="User Name" required> 
-    <input type ="text" name = "password" palceholder="Password Name" required> 
-    <input type ="text" name = "email" palceholder="Email Id" required> 
+    <input type="text" name = "firstName" placeholder="First Name" requrired ><br>
+    <input type ="text" name = "lastName" palceholder="Last Name" required> <br>
+    <input type ="text" name = "userName" palceholder="User Name" required> <br>
+    <input type ="text" name = "password" palceholder="Password Name" required> <br>
+    <input type ="text" name = "email" palceholder="Email Id" required> <br>
     <input type ="text" name = "phoneNumber" palceholder="Phone Number" required> 
     <input type ="submit" name = "register" value = "register">
 </body>
